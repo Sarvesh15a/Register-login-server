1730c9d2f68c8431d4d13212ff0e914289580def
Sarvesh15a <91020312+Sarvesh15a@users.noreply.github.com>
2024-01-08 16:05:29 +0530

    Add files via upload

---
+++ mongo.js
@@ -0,0 +1,30 @@
+
+
+const mongoose=require("mongoose")
+mongoose.connect("mongodb+srv://yadsarvesh15:sarvesh@cluster0.xal9y1c.mongodb.net/")
+.then(()=>{
+    console.log("mongodb connected");
+})
+.catch(()=>{
+    console.log('failed');
+})
+
+
+const newSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+const collection = mongoose.model("collection",newSchema)
+
+module.exports=collection
